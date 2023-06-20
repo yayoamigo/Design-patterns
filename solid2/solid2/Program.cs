e4dd3e1142497653b55401a4994cec04ebd419f6
yayoamigo <david._101@hotmail.com>
2023-06-20 10:41:18 -0500

    interface segragation principle

---
+++ solid2/solid2/Program.cs
@@ -0,0 +1,95 @@
+﻿//No client should be force to depend on methods it doesn use
+using System;
+
+// Bad design violating ISP
+interface IWorker
+{
+    void Work();
+    void Eat();
+}
+
+class Worker : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Working...");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+class Manager
+{
+    private readonly IWorker worker;
+
+    public Manager(IWorker worker)
+    {
+        this.worker = worker;
+    }
+
+    public void Manage()
+    {
+        worker.Work();
+        worker.Eat();
+    }
+}
+
+// Good design following ISP
+interface IWorkable
+{
+    void Work();
+}
+
+interface IEatable
+{
+    void Eat();
+}
+
+class Worker2 : IWorkable, IEatable
+{
+    public void Work()
+    {
+        Console.WriteLine("Working...");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+class Manager2
+{
+    private readonly IWorkable worker;
+
+    public Manager2(IWorkable worker)
+    {
+        this.worker = worker;
+    }
+
+    public void Manage()
+    {
+        worker.Work();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bad design
+        IWorker worker = new Worker();
+        Manager manager = new Manager(worker);
+        manager.Manage();
+
+        Console.WriteLine();
+
+        // Good design
+        IWorkable worker2 = new Worker2();
+        Manager2 manager2 = new Manager2(worker2);
+        manager2.Manage();
+    }
+}
