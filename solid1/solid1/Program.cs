bfff82100037b197e628cea47552273f67bce8d5
yayoamigo <david._101@hotmail.com>
2023-06-20 10:12:56 -0500

    single

---
+++ solid1/solid1/Program.cs
@@ -0,0 +1,17 @@
+﻿//means that each class or interface should be concercn with only one thing should have just one responsability
+
+interface Iuser
+{
+    bool Login(string username, string password);
+    bool Register(string username, string password, string email);
+}
+
+interface Iloger
+{
+    void Error(string message);
+}
+
+interface Iemail
+{
+    bool SendEmail(string emailContent);
+}
