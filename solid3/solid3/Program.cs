fdbacd2bf0f413c20b7e4799f5998b6c53640940
yayoamigo <david._101@hotmail.com>
2023-06-21 10:07:48 -0500

    Open Close

---
+++ solid3/solid3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace openClose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PermanentEmployee john = new PermanentEmployee(1, "John");
+            TempEmployee david = new TempEmployee(2, "David");
+
+            Console.WriteLine($"employee{john.ToString()}, John Bonus: {john.CalculateBonus(10000).ToString()}");
+            Console.WriteLine($"employee{david.ToString()}, David Bonus: {david.CalculateBonus(15000).ToString()}");
+        }
+    }
+}
