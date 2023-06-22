7fa22d922fb20a7d578774776a2a3b5e2525b3b7
yayoamigo <david._101@hotmail.com>
2023-06-22 16:45:52 -0500

    Dependency inversion

---
+++ solid4/solid4/Program.cs
@@ -0,0 +1,23 @@
+﻿using DependencyInv;
+using System;
+
+namespace solid4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson David = Factory.CreatePerson("David", 31, "david@gmail.com");
+
+            IChore chore1 = Factory.CreateChore("Take out the trash", David);
+
+            chore1.PerformedChore(3);
+            chore1.PerformedChore(1);
+            chore1.CompleteChore();
+
+            int hoursWorked = chore1.HoursWorked;
+
+            Console.Write(hoursWorked);
+        }
+    }
+}
